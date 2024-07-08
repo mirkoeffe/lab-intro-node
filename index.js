8cb634a51085761edba38030a9e9b71ad7965476
Mirko Fede <mirko@mirkoeffe.shop>
2024-07-08 14:27:26 +0200

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    } return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("Empty SortedList");
+    } return Math.max(...this.items);
+  }
 
-  sum() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("Empty SortedList");
+    } return Math.min(...this.items);
+  }
 
-  avg() {}
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    } return this.items.reduce((a, b) => a + b, 0);
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error("Empty SortedList");
+    } return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
